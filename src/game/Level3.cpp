fb4957db80598eefae42c0b679165e10dd002718
Timothy003 <Timothy003@localhost>
2010-06-12 20:17:13 -0400

    allow 0 as skill levels in setskill command

--- src/game/Level3.cpp
+++ src/game/Level3.cpp
@@ -1771,7 +1771,7 @@ bool ChatHandler::HandleSetSkillCommand(const char *args)
 
     int32 max   = max_p ? atol (max_p) : target->GetPureMaxSkillValue(skill);
 
-    if (level <= 0 || level > max || max <= 0)
+    if (level < 0 || level > max || max < 0)
         return false;
 
     target->SetSkill(skill, target->GetSkillStep(skill), level, max);
