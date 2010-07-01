f58889f4d3354ef935909ea52d8de75a0d007ab7
Timothy003 <Timothy003@localhost>
2010-07-01 00:46:57 -0400

    fixed pet name bug

---
+++ src/server/scripts/EasternKingdoms/ZulAman/zulaman.h
@@ -0,0 +1,23 @@
+/* Copyright (C) 2006 - 2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+* This program is free software licensed under GPL version 2
+* Please see the included DOCS/LICENSE.TXT for more information */
+
+#ifndef DEF_ZULAMAN_H
+#define DEF_ZULAMAN_H
+
+enum eData
+{
+    DATA_NALORAKKEVENT = 1,
+    DATA_AKILZONEVENT  = 2,
+    DATA_JANALAIEVENT  = 3,
+    DATA_HALAZZIEVENT  = 4,
+    DATA_HEXLORDEVENT  = 5,
+    DATA_ZULJINEVENT   = 6,
+    DATA_CHESTLOOTED   = 7
+};
+
+#define TYPE_RAND_VENDOR_1  8
+#define TYPE_RAND_VENDOR_2  9
+
+#endif
+
