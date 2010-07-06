3456c1229680d1435ecbfaefe9e352444a60ab20
Timothy003 <Timothy003@localhost>
2010-07-06 12:20:06 -0400

    fixed precompiled header usage with game and scripts on MSVC

--- src/server/game/Events/GameEventMgr.cpp
+++ src/server/game/Events/GameEventMgr.cpp
@@ -18,6 +18,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include "gamePCH.h"
 #include "GameEventMgr.h"
 #include "World.h"
 #include "ObjectMgr.h"
--- src/server/game/Movement/MovementGenerators/WaypointMovementGenerator.cpp
+++ src/server/game/Movement/MovementGenerators/WaypointMovementGenerator.cpp
@@ -16,6 +16,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 //Basic headers
+#include "gamePCH.h"
 #include "WaypointMovementGenerator.h"
 #include "DestinationHolderImp.h"
 //Extended headers
