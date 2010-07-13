eca08e07bc9e07adebf8fb8508129eabb56a30c8
Timothy003 <Timothy003@localhost>
2010-07-13 07:56:39 -0400

    fixed cset 7a1ac43569e0b44c2e69befaf5036b6b7b733ed3

--- src/server/scripts/Northrend/Ulduar/ulduar/boss_xt002.cpp
+++ src/server/scripts/Northrend/Ulduar/ulduar/boss_xt002.cpp
@@ -131,7 +131,9 @@ enum Yells
  *///----------------------------------------------------
 struct boss_xt002_AI : public BossAI
 {
-    boss_xt002_AI(Creature *pCreature) : BossAI(pCreature, BOSS_XT002), vehicle(me->GetVehicleKit())
+    boss_xt002_AI(Creature *pCreature)
+        : BossAI(pCreature, boss_xt002),
+            vehicle(me->GetVehicleKit())
     {
         assert(vehicle);
         pInstance = pCreature->GetInstanceData();
@@ -491,7 +493,8 @@ struct mob_xt002_heartAI : public ScriptedAI
     void JustDied(Unit *victim)
     {
         if (m_pInstance)
-            if (Creature* pXT002 = me->GetCreature(*me, m_pInstance->GetData64(TYPE_XT002)))
+            if (Creature
+                    *pXT002 = me->GetCreature(*me, m_pInstance->GetData64(data64_xt002)))
                 if (pXT002->AI())
                     pXT002->AI()->DoAction(ACTION_ENTER_HARD_MODE);
 
@@ -520,7 +523,8 @@ struct mob_xt002_heartAI : public ScriptedAI
     {
         if (m_pInstance)
         {
-            if (Creature* pXT002 = me->GetCreature(*me, m_pInstance->GetData64(TYPE_XT002)))
+            if (Creature
+                    *pXT002 = me->GetCreature(*me, m_pInstance->GetData64(data64_xt002)))
             {
                 if (pXT002->AI())
                 {
@@ -555,7 +559,8 @@ struct mob_scrapbotAI : public ScriptedAI
         repaired = false;
 
         if (m_pInstance)
-            if (Creature* pXT002 = me->GetCreature(*me, m_pInstance->GetData64(TYPE_XT002)))
+            if (Creature
+                    *pXT002 = me->GetCreature(*me, m_pInstance->GetData64(data64_xt002)))
                 me->AI()->AttackStart(pXT002);
     }
 
@@ -564,7 +569,8 @@ struct mob_scrapbotAI : public ScriptedAI
         if (!m_pInstance)
             return;
 
-        if (Creature* pXT002 = me->GetCreature(*me, m_pInstance->GetData64(TYPE_XT002)))
+        if (Creature
+                *pXT002 = me->GetCreature(*me, m_pInstance->GetData64(data64_xt002)))
         {
             if (!repaired && me->GetDistance2d(pXT002) <= 0.5)
             {
@@ -651,13 +657,15 @@ struct mob_boombotAI : public ScriptedAI
     
     void Reset()
     {
-        if (Creature* pXT002 = me->GetCreature(*me, m_pInstance->GetData64(TYPE_XT002)))
+        if (Creature
+                *pXT002 = me->GetCreature(*me, m_pInstance->GetData64(data64_xt002)))
             me->AI()->AttackStart(pXT002);
     }
     
     void UpdateAI(const uint32 diff)
     {
-        if (Creature* pXT002 = me->GetCreature(*me, m_pInstance->GetData64(TYPE_XT002)))
+        if (Creature
+                *pXT002 = me->GetCreature(*me, m_pInstance->GetData64(data64_xt002)))
         {
             if (me->GetDistance2d(pXT002) <= 0.5 || HealthBelowPct(50))
             {
