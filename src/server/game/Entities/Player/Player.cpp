e3f69e2392844f4aca15665036a55f6f500e27d1
Timothy003 <Timothy003@localhost>
2010-07-19 03:34:20 -0400

    fixed phase bug when being ejected from instances

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -19975,7 +19975,6 @@ void Player::UpdateHomebindTime(uint32 time)
         if (time >= m_HomebindTimer)
         {
             // teleport to nearest graveyard
-            SetPhaseMask(1,true);
             RepopAtGraveyard();
         }
         else
@@ -19991,7 +19990,6 @@ void Player::UpdateHomebindTime(uint32 time)
         data << uint32(1);
         GetSession()->SendPacket(&data);
         sLog.outDebug("PLAYER: Player '%s' (GUID: %u) will be teleported to homebind in 60 seconds", GetName(),GetGUIDLow());
-        SetPhaseMask(2,true);
     }
 }
 
