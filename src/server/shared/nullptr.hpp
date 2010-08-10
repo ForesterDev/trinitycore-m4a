c89d652b06f152cea56860a6606728fcedc00c05
Timothy003 <Timothy003@localhost>
2010-08-10 02:31:00 -0400

    GCC workaround for std::function and std::nullptr_t overloads

--- src/server/shared/nullptr.hpp
+++ src/server/shared/nullptr.hpp
@@ -1,12 +1,23 @@
 #pragma once
 
 #ifdef __GNUC__
+#if __GNUC_MINOR__ < 6
+
+#include <cstddef>
+#include <type_traits>
 
 namespace std
 {
+    struct _M_clear_type;
+
     class nullptr_t
     {   // typedef for decltype(nullptr)
     public:
+        operator _M_clear_type *() const
+        {   // return null pointer
+            return NULL;
+        }
+
         template<class _ty>
             operator _ty *() const
         {   // return null pointer
@@ -19,8 +30,15 @@ namespace std
             return 0;
         }
     };
+
+    template<>
+        struct is_integral<nullptr_t>
+        : public true_type
+    {   // make function work with nullptr_t
+    };
 }
 
 #define nullptr ::std::nullptr_t()
 
+#endif  // __GNUC_MINOR__ < 6
 #endif  // __GNUC__
