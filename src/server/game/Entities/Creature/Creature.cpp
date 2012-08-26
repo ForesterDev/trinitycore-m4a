513cf9c9b62c81e8f720c1febd44f05eb7d2311d
Timothy Liang <timothy003@msn.com>
2012-08-26 12:11:15 -0400

    fix rotface's slime spray

--- src/server/game/Entities/Creature/Creature.cpp
+++ src/server/game/Entities/Creature/Creature.cpp
@@ -449,6 +449,9 @@ bool Creature::UpdateEntry(uint32 Entry, uint32 team, const CreatureData* data)
 
 void Creature::Update(uint32 diff)
 {
+    if (auto target = HasUnitState(UNIT_STATE_TRACKING_CHANNEL_OBJECT) ? GetUInt64Value(UNIT_FIELD_CHANNEL_OBJECT) : GetUInt64Value(UNIT_FIELD_TARGET))
+        if (auto unit = ObjectAccessor::GetUnit(*this, target))
+            SetInFront(unit);
     if (IsAIEnabled && TriggerJustRespawned)
     {
         TriggerJustRespawned = false;
