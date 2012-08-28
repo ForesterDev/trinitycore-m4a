e70016c82556c9fb99aa104a998a5979fabd2eb7
Timothy Liang <timothy003@msn.com>
2012-08-28 09:29:49 -0400

    fix valanar's kinetic bomb

--- src/server/scripts/Northrend/IcecrownCitadel/boss_blood_prince_council.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/boss_blood_prince_council.cpp
@@ -1006,11 +1006,8 @@ class boss_prince_valanar_icc : public CreatureScript
                             Talk(SAY_VALANAR_BERSERK);
                             break;
                         case EVENT_KINETIC_BOMB:
-                            if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 1, 0.0f, true))
-                            {
-                                DoCast(target, SPELL_KINETIC_BOMB_TARGET);
-                                Talk(SAY_VALANAR_SPECIAL);
-                            }
+                            DoCastAOE(SPELL_KINETIC_BOMB_TARGET);
+                            Talk(SAY_VALANAR_SPECIAL);
                             events.ScheduleEvent(EVENT_KINETIC_BOMB, urand(18000, 24000));
                             break;
                         case EVENT_SHOCK_VORTEX:
