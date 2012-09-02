c676de832a8f0c5a80561236e832b94a0f801eab
Timothy Liang <timothy003@msn.com>
2012-09-02 02:25:49 -0400

    Backed out changeset: a8b1aefab439

--- src/server/scripts/Northrend/IcecrownCitadel/boss_professor_putricide.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/boss_professor_putricide.cpp
@@ -855,15 +855,6 @@ class spell_putricide_ooze_channel : public SpellScriptLoader
 
             void SelectTarget(std::list<WorldObject*>& targets)
             {
-                switch (GetSpellInfo()->ExcludeTargetAuraSpell)
-                {
-                case 70530 /* Volatile Ooze Beam Protection */:
-                    targets.remove_if(Trinity::UnitAuraCheck(true, SPELL_GAS_VARIABLE));
-                    break;
-                case 70812 /* Gaseous Bloat Protection */:
-                    targets.remove_if(Trinity::UnitAuraCheck(true, SPELL_OOZE_VARIABLE));
-                    break;
-                }
                 if (targets.empty())
                 {
                     FinishCast(SPELL_FAILED_NO_VALID_TARGETS);
