9d332c455719762c98cc94d5a907a5bce4715ae7
Mladen Ilic <mladens.world@gmail.com>
2012-09-03 20:46:11 +0200

    Festergut - cleanup

---
+++ src/server/game/PrecompiledHeaders/gamePCH.h
@@ -0,0 +1,28 @@
+//add here most rarely modified headers to speed up debug build compilation
+
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <memory>
+#include <algorithm>
+#include <utility>
+#include <functional>
+#include <array>
+#include <limits>
+#include <sstream>
+#include <boost/math/special_functions/round.hpp>
+#include <boost/none.hpp>
+#include <boost/numeric/conversion/cast.hpp>
+#include <boost/optional/optional.hpp>
+#include <boost/thread/locks.hpp>
+#include "WorldSocket.h"        // must be first to make ACE happy with ACE includes in it
+
+#include "Common.h"
+
+#include "MapManager.h"
+#include "Log.h"
+#include "ObjectAccessor.h"
+#include "ObjectDefines.h"
+#include "Opcodes.h"
+#include "SharedDefines.h"
+#include "ObjectMgr.h"
