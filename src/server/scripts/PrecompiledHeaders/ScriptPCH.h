5f65a39e0f6d3c74de18fbd9b43b482b126feb64
Mladen Ilic <mladens.world@gmail.com>
2012-09-04 23:57:45 +0200

    Rotface - Mutated infection triggers

---
+++ src/server/scripts/PrecompiledHeaders/ScriptPCH.h
@@ -0,0 +1,44 @@
+/* Copyright (C) 2006 - 2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+* This program is free software licensed under GPL version 2
+* Please see the included DOCS/LICENSE.TXT for more information */
+
+#ifndef SC_PRECOMPILED_H
+#define SC_PRECOMPILED_H
+
+#include <algorithm>
+#include <array>
+#include <functional>
+#include <iterator>
+#include <memory>
+#include <list>
+#include <stdexcept>
+#include <typeinfo>
+#include <utility>
+#include <vector>
+#include <boost/numeric/conversion/cast.hpp>
+#include "ScriptMgr.h"
+#include "Cell.h"
+#include "CellImpl.h"
+#include "GameEventMgr.h"
+#include "GridNotifiers.h"
+#include "GridNotifiersImpl.h"
+#include "Unit.h"
+#include "GameObject.h"
+#include "ScriptedCreature.h"
+#include "ScriptedGossip.h"
+#include "InstanceScript.h"
+#include "CombatAI.h"
+#include "PassiveAI.h"
+#include "Chat.h"
+#include "DBCStructure.h"
+#include "DBCStores.h"
+#include "ObjectMgr.h"
+#include "SpellScript.h"
+#include "SpellAuraEffects.h"
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+#endif
+
