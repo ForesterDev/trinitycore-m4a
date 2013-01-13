c0dc5396326dc95ecd87cd5664bd49e324828cba
joschiwald <joschiwald@online.de>
2013-01-13 02:41:40 +0100

    Misc: Reorder more scripts in some script files and some cosmetic stuff

---
+++ src/server/scripts/Outland/TempestKeep/Mechanar/instance_mechanar.cpp
@@ -0,0 +1,170 @@
+/*
+ * Copyright (C) 2008-2013 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2006-2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* ScriptData
+SDName: Instance_Mechanar
+SD%Complete: 100
+SDComment:
+SDCategory: Mechanar
+EndScriptData */
+
+#include "ScriptMgr.h"
+#include "InstanceScript.h"
+#include "mechanar.h"
+
+class instance_mechanar : public InstanceMapScript
+{
+    public: instance_mechanar(): InstanceMapScript("instance_mechanar", 554) { }
+
+        InstanceScript* GetInstanceScript(InstanceMap* map) const
+        {
+            return new instance_mechanar_InstanceMapScript(map);
+        }
+
+        struct instance_mechanar_InstanceMapScript : public InstanceScript
+        {
+            instance_mechanar_InstanceMapScript(Map* map) : InstanceScript(map)
+            {
+                SetBossNumber(EncounterCount);
+                DoorMoArg1GUID          = 0;
+                DoorMoArg2GUID          = 0;
+                DoorNethermancerGUID    = 0;
+            }
+
+            void OnGameObjectCreate(GameObject* go)
+            {
+                switch (go->GetEntry())
+                {
+                    case GO_DOOR_MOARG_1:
+                        DoorMoArg1GUID = go->GetGUID();
+                        break;
+                    case GO_DOOR_MOARG_2:
+                        DoorMoArg2GUID = go->GetGUID();
+                        break;
+                    case GO_DOOR_NETHERMANCER:
+                        DoorNethermancerGUID = go->GetGUID();
+                        break;
+                    default:
+                        break;
+                }
+                CheckInstanceStatus();
+            }
+
+            void CheckInstanceStatus()
+            {
+                if (GetBossState(DATA_GATEWATCHER_IRON_HAND) == DONE)
+                    HandleGameObject(DoorMoArg1GUID, true);
+
+                if (GetBossState(DATA_GATEWATCHER_GYROKILL) == DONE)
+                    HandleGameObject(DoorMoArg2GUID, true);
+
+                if (GetBossState(DATA_NETHERMANCER_SEPRETHREA) == DONE)
+                    HandleGameObject(DoorNethermancerGUID, true);
+            }
+
+            uint64 GetData64(uint32 type) const
+            {
+                switch (type)
+                {
+                    case GO_DOOR_MOARG_1:
+                        return DoorMoArg1GUID;
+                    case GO_DOOR_MOARG_2:
+                        return DoorMoArg2GUID;
+                    case GO_DOOR_NETHERMANCER:
+                        return DoorNethermancerGUID;
+                    default:
+                        break;
+                }
+
+                return 0;
+            }
+
+            bool SetBossState(uint32 type, EncounterState state)
+            {
+                if (!InstanceScript::SetBossState(type, state))
+                    return false;
+
+                switch (type)
+                {
+                    case DATA_GATEWATCHER_GYROKILL:
+                    case DATA_GATEWATCHER_IRON_HAND:
+                    case DATA_MECHANOLORD_CAPACITUS:
+                    case DATA_NETHERMANCER_SEPRETHREA:
+                    case DATA_PATHALEON_THE_CALCULATOR:
+                        break;
+                    default:
+                        break;
+                }
+
+                return true;
+            }
+
+            std::string GetSaveData()
+            {
+                OUT_SAVE_INST_DATA;
+
+                std::ostringstream saveStream;
+                saveStream << "M C " << GetBossSaveData();
+
+                OUT_SAVE_INST_DATA_COMPLETE;
+                return saveStream.str();
+            }
+
+            void Load(const char* str)
+            {
+                if (!str)
+                {
+                    OUT_LOAD_INST_DATA_FAIL;
+                    return;
+                }
+
+                OUT_LOAD_INST_DATA(str);
+
+                char dataHead1, dataHead2;
+
+                std::istringstream loadStream(str);
+                loadStream >> dataHead1 >> dataHead2;
+
+                if (dataHead1 == 'M' && dataHead2 == 'C')
+                {
+                    for (uint32 i = 0; i < EncounterCount; ++i)
+                    {
+                        uint32 tmpState;
+                        loadStream >> tmpState;
+                        if (tmpState == IN_PROGRESS || tmpState > SPECIAL)
+                            tmpState = NOT_STARTED;
+                        SetBossState(i, EncounterState(tmpState));
+                    }
+                }
+                else
+                    OUT_LOAD_INST_DATA_FAIL;
+
+                OUT_LOAD_INST_DATA_COMPLETE;
+            }
+
+        protected:
+            uint64 DoorMoArg1GUID;
+            uint64 DoorMoArg2GUID;
+            uint64 DoorNethermancerGUID;
+        };
+};
+
+void AddSC_instance_mechanar()
+{
+    new instance_mechanar();
+}
