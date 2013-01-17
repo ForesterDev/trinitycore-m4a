4d9b097964eab0b8c773b6735646c5a5fa2d7634
Mladen Ilic <mladens.world@gmail.com>
2013-01-17 18:55:02 +0100

    Removed wg update sql

--- sql/updates/world/2013_01_17_00_world_npc_vendor_wgmarks.sql
+++
@@ -1,3 +0,0 @@
--- Adding Wintergrasp Mark of Honor to WG vendors
-INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`) VALUES (39172, 0, 43589, 0, 0, 427);
-INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`) VALUES (39173, 0, 43589, 0, 0, 427);
