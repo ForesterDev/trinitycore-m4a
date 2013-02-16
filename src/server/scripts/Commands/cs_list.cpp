e8e505ca7f722d8ec8a7c1be641a7fb48da23f12
Timothy Liang <timothy003@msn.com>
2013-02-16 05:49:17 -0500

    remove boost includes from precompiled headers
    fixes intellisense for most files

--- src/server/scripts/Commands/cs_list.cpp
+++ src/server/scripts/Commands/cs_list.cpp
@@ -31,8 +31,6 @@ EndScriptData */
 #include "ObjectMgr.h"
 #include "Player.h"
 
-using boost::optional;
-
 class list_commandscript : public CommandScript
 {
 public:
@@ -446,7 +444,7 @@ public:
             ss_name << "|cffffffff|Hspell:" << aura->GetId() << "|h[" << name << "]|h|r";
 
             handler->PSendSysMessage(LANG_COMMAND_TARGET_AURADETAIL, aura->GetId(), (handler->GetSession() ? ss_name.str().c_str() : name),
-                aurApp->GetEffectMask(), aura->GetCharges(), aura->GetStackAmount(), optional<int>(aurApp->GetSlot()).get_value_or(-1),
+                aurApp->GetEffectMask(), aura->GetCharges(), aura->GetStackAmount(), aurApp->GetSlot().first ? aurApp->GetSlot().second : -1,
                 aura->GetDuration(), aura->GetMaxDuration(), (aura->IsPassive() ? passiveStr : ""),
                 (talent ? talentStr : ""), IS_PLAYER_GUID(aura->GetCasterGUID()) ? "player" : "creature",
                 GUID_LOPART(aura->GetCasterGUID()));
