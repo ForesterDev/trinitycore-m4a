d4d4607ccb2d042a61b40d5995ed0d5b36915373
Timothy Liang <timothy003@msn.com>
2013-03-17 15:32:11 -0400

    add z position offset to strangulate vehicle

---
+++ sql/m4a-updates/world/2013_03_17_00_world_spell_script_names.sql
@@ -0,0 +1,1 @@
+replace into spell_script_names (spell_id, ScriptName) values (68986,'spell_the_lich_king_harvest_soul_summon');
---
+++ sql/m4a-updates/world/2013_03_17_01_world_creature_template.sql
@@ -0,0 +1,1 @@
+update creature_template set InhabitType=0x4 where entry=36598;
